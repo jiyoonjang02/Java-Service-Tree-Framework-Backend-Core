f703fe7c125b3d21a7928318d3d2bf4986ebd09f
LeeDongMin <admin@313.co.kr>
2023-03-16 10:11:05 +0900

    [SP-313] #comment treeframework - 스웨거 처리 방안 마련 완료 ( 디테일 처리는 필요한 상황 )  23.03.15 #close #time 1h +review SR @dongmin.lee
    [ Backend-Core :: PLE-TreeFramework :: 313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- src/main/java/com/config/Swagger2Config.java
+++ src/main/java/com/config/Swagger2Config.java
@@ -52,19 +52,19 @@ public class Swagger2Config {
         return new ApiInfoBuilder()
                 .title("PLE TreeFramework")
                 .description(
-                    "<span style='color:blue'><strong>&#8880; Select &trade;&#8881;</strong></span>\n\n" +
+                    "<span style='color: #e5603b;font-size: 18px;'><strong>&#8880; Select &trade;&#8881;</strong></span>\n\n" +
 
-                    "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color:blue'>[ getNode.do ]</span>\n\n" +
-                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style='color:blue'>&copy;c_id</span> -> ( c_id 값의 Node Return )\n\n" +
+                    "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #E49400; font-size: 16px;'>[ getNode.do ]</span>\n\n" +
+                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #2477FF; font-size: 14px;'>&copy; c_id</span> -> ( c_id 값의 Node Return )\n\n" +
 
-                    "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color:blue'>[ getChildNode.do ]</span>\n\n" +
-                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style='color:blue'>&copy;c_id</span> -> ( c_id 값의 Node 하위 ( 자식 ) Node Return )\n\n" +
+                    "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #E49400; font-size: 16px;'>[ getChildNode.do ]</span>\n\n" +
+                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #2477FF; font-size: 14px;'>&copy; c_id</span> -> ( c_id 값의 Node 하위 ( 자식 ) Node Return )\n\n" +
 
-                    "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color:blue'>[ getPaginatedChildNode.do ]</span>\n\n" +
-                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style='color:blue'>&copy;c_id</span> -> ( c_id 값의 Node 하위 ( 자식 ) Node를 페이징 처리하여 Return )\n\n" +
+                    "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #E49400; font-size: 16px;'>[ getPaginatedChildNode.do ]</span>\n\n" +
+                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #2477FF; font-size: 14px;'>&copy; c_id</span> -> ( c_id 값의 Node 하위 ( 자식 ) Node를 페이징 처리하여 Return )\n\n" +
 
                     "<br><br>" +
-                    "<span style='color:blue'><strong>&#8880; Insert&trade; &#8881;</strong></span>\n\n" +
+                    "<span style='color: #e5603b;font-size: 15px;'><strong>&#8880; Insert&trade; &#8881;</strong></span>\n\n" +
                     "<span style='color:blue'>[addNode.do]</span>\n\n" +
                     "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color:blue'>&copy;ref</span> -> ( 어느 부모 아이디 밑으로 추가할 건지 )\n\n" +
                     "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color:blue'>&copy;c_position</span> -> ( 추가될 때 어느 포지션 위치 값으로 들어 갈 건지 )\n\n" +
