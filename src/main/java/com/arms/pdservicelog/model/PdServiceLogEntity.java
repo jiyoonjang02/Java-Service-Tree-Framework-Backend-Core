3dd44d79ae2f5db003b36d6bc1663e4c6a977ab9
LeeDongMin <admin@313.co.kr>
2023-03-21 14:03:58 +0900

    [SP-313] #comment treeframework - PdServiceLog 23.03.21 #close #time 1h +review SR @dongmin.lee
    [ Backend-Core :: PLE-TreeFramework :: 313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

---
+++ src/main/java/com/arms/pdservicelog/model/PdServiceLogEntity.java
@@ -0,0 +1,135 @@
+/*
+ * @author Dongmin.lee
+ * @since 2023-03-21
+ * @version 23.03.21
+ * @see <pre>
+ *  Copyright (C) 2007 by 313 DEV GRP, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by 313 developer group <313@313.co.kr>, December 2010
+ * </pre>
+ */
+package com.arms.pdservicelog.model;
+
+import com.egovframework.ple.treeframework.model.TreeBaseEntity;
+import com.egovframework.ple.treeframework.model.TreeSearchEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "T_ARMS_PDSERVICE_LOG")
+@SelectBeforeUpdate(value=true)
+@DynamicInsert(value=true)
+@DynamicUpdate(value=true)
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class PdServiceLogEntity extends TreeSearchEntity implements Serializable {
+
+    public PdServiceLogEntity() {
+        super();
+    }
+
+    public PdServiceLogEntity(Boolean copyBooleanValue) {
+        super();
+        this.copyBooleanValue = copyBooleanValue;
+    }
+
+ 	@Override
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "c_id")
+    public Long getC_id() {
+        return super.getC_id();
+    }
+
+    //@Getter @Setter
+    @Lob
+    @Column(name="c_contents")
+    private String c_contents;
+
+    @Column(name="c_etc")
+    @Type(type="text")
+    private String c_etc;
+
+    @Column(name="c_owner")
+    @Type(type="text")
+    private String c_owner;
+
+    @Column(name="c_reviewer01")
+    @Type(type="text")
+    private String c_reviewer01;
+
+    @Column(name="c_reviewer02")
+    @Type(type="text")
+    private String c_reviewer02;
+
+    @Column(name="c_reviewer03")
+    @Type(type="text")
+    private String c_reviewer03;
+
+    @Column(name="c_reviewer04")
+    @Type(type="text")
+    private String c_reviewer04;
+
+    @Column(name="c_reviewer05")
+    private String c_reviewer05;
+
+    @Column(name="c_writer_name")
+    @Type(type="text")
+    private String c_writer_name;
+
+    @Column(name="c_writer_cn")
+    @Type(type="text")
+    private String c_writer_cn;
+
+    @Column(name="c_writer_mail")
+    @Type(type="text")
+    private String c_writer_mail;
+
+    @Column(name="c_writer_date")
+    @Type(type="text")
+    private String c_writer_date;
+
+    @Column(name="c_fileid_link")
+    private Long c_fileid_link;
+    /*
+     * Extend Bean Field
+     */
+	@JsonIgnore
+    private Boolean copyBooleanValue;
+
+    @Transient
+	@ApiModelProperty(hidden = true)
+    public Boolean getCopyBooleanValue() {
+        copyBooleanValue = false;
+        if (this.getCopy() == 0) {
+            copyBooleanValue = false;
+        } else {
+            copyBooleanValue = true;
+        }
+        return copyBooleanValue;
+    }
+
+    public void setCopyBooleanValue(Boolean copyBooleanValue) {
+        this.copyBooleanValue = copyBooleanValue;
+    }
+
+    @Override
+    public <T extends TreeSearchEntity> void setFieldFromNewInstance(T paramInstance) {
+        if( paramInstance instanceof TreeBaseEntity){
+            if(paramInstance.isCopied()) {
+                this.setC_title("copy_" + this.getC_title());
+            }
+        }
+    }
+}
